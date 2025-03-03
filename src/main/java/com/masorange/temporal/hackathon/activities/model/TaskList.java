be4589f7552474e2ffef5302d795226f730c44eb
eduardo-hornos <eduardo.hornosm@masmovil.com>
2025-03-03 14:09:54 +0000

    activity openai

---
+++ src/main/java/com/masorange/temporal/hackathon/activities/model/TaskList.java
@@ -0,0 +1,14 @@
+package com.masorange.temporal.hackathon.activities.model;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TaskList {
+
+  private List<Task> tasks;
+}
--- src/main/java/com/masorange/temporal/hackathon/workflows/IntelligentAgendaScheduler.java
+++ src/main/java/com/masorange/temporal/hackathon/workflows/IntelligentAgendaScheduler.java
@@ -1,6 +1,7 @@
 package com.masorange.temporal.hackathon.workflows;
 
-import com.masorange.temporal.hackathon.activities.MessagesActivities;
+import com.masorange.temporal.hackathon.activities.OpenAIActivity;
+import com.masorange.temporal.hackathon.activities.SlackActivity;
 
 import java.time.Duration;
 import java.time.OffsetDateTime;
@@ -11,6 +12,7 @@ import io.temporal.workflow.Workflow;
 import io.temporal.workflow.WorkflowInterface;
 import io.temporal.workflow.WorkflowMethod;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.IOUtils;
 
 @WorkflowInterface
 public interface IntelligentAgendaScheduler {
@@ -34,14 +36,23 @@ public interface IntelligentAgendaScheduler {
         .setScheduleToCloseTimeout(Duration.ofSeconds(5000))
         .build();
 
-    private final MessagesActivities channelMessages =
-        Workflow.newActivityStub(MessagesActivities.class, defaultActivityOptions);
+    private final SlackActivity channelMessages =
+        Workflow.newActivityStub(SlackActivity.class, defaultActivityOptions);
+
+    private final OpenAIActivity openAIActivity = Workflow.newActivityStub(OpenAIActivity.class,
+        defaultActivityOptions);
 
     @Override
     public void summarizeSlackChannelConversations() {
       // Do something awesome
-      var messages = channelMessages.retrieveMessages(OffsetDateTime.now().minusDays(1));
-      log.debug("Messages from channel: {}", messages);
+//      var messages = channelMessages.retrieveMessages("provision", OffsetDateTime.now().minusDays(1));
+      try {
+        var taskList = openAIActivity.calculateTasks(
+            IOUtils.toString(IntelligentAgendaScheduler.class.getResourceAsStream("/user-message.txt")));
+        log.debug("Messages from channel: {}", taskList);
+      } catch (Exception e) {
+        log.error("Error retrieving messages from channel", e);
+      }
     }
   }
 }
